80eae850dab968fd285d3979c70625bf89a16076
amgross <aryespa@gmail.com>
2024-03-28 13:25:57 +0200

    Add file id utility

--- my_code.c
+++ my_code.c
@@ -7,6 +7,8 @@
 
 
 #define MY_STR "hello world\n"
+#define MY_STR2 "world hello\n"
+
 
 #define MEM_SIZE 0x100
 
@@ -17,6 +19,16 @@ typedef struct my_code
 	int size;
 	char data[];
 }file;
+
+typedef enum
+{
+	helfs_success,
+	helfs_mem_err, // Out of memory
+	helfs_boundaries_err, // Out of boundaries
+	halfs_param_err, // General parameter error
+}helfs_ret;
+
+typedef uint32_t file_id;
 
 int init_fs()
 {
@@ -42,27 +54,38 @@ file *find_empty_place(int size)
 	}
 }
 
-int create_and_write(char *in, int size)
+helfs_ret create_and_write(char *in, int size, file_id *out_id)
 {
-	file *new_file = find_empty_place(size);
+	file *new_file;
+
+	if(NULL == out_id)
+	{
+		return halfs_param_err;
+	}
+	
+	new_file = find_empty_place(size);
 	if(new_file == NULL)
 	{
-		return -1;
+		return helfs_mem_err;
 	}
 
 	new_file->size = size;
 
 	memcpy(new_file->data, in, size);
 
+	*out_id = (uint8_t *)new_file - mem_buff;
+
 	return 0;
 }
 
-int read_file(char *out, int size)
+helfs_ret read_file(file_id id, char *out, int size)
 {
-	file *read_file = (file *)mem_buff;
+	file *read_file = (file *)(mem_buff + id);
+
+	// TODO need much more checks to ensure we are not out of boundaries
 	if(read_file->size < size)
 	{
-		return -1;
+		return helfs_boundaries_err;
 	}
 
 	memcpy(out, read_file->data, size);
@@ -73,43 +96,46 @@ int read_file(char *out, int size)
 
 void basic_test()
 {
-	int ret;
+	file_id id;
+	helfs_ret ret;
 	char buff[100];
 	buff[0] = 0;
 
 	ret = init_fs();
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
-	ret = create_and_write(MY_STR, sizeof(MY_STR));
+	ret = create_and_write(MY_STR, sizeof(MY_STR), &id);
 	TEST_ASSERT_(ret == 0, "got error %d", ret);
 
-	ret = read_file(buff, sizeof(MY_STR));
+	ret = read_file(id, buff, sizeof(MY_STR));
 	TEST_ASSERT_(ret == 0, "got error %d", ret);
 
 	TEST_ASSERT(memcmp(buff, MY_STR, sizeof(MY_STR)) == 0);
 }
 
-void write_to_big_test()
+void write_too_big_test()
 {
-	int ret;
+	file_id id;
+	helfs_ret ret;
 	char buff[MEM_SIZE * 2];
 
 	ret = init_fs();
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
-	ret = create_and_write(buff, sizeof(buff));
-	TEST_ASSERT_(ret == -1, "expected error -1 but got %d", ret);
+	ret = create_and_write(buff, sizeof(buff), &id);
+	TEST_ASSERT_(ret == helfs_mem_err, "expected error helfs_mem_err-%d but got %d", helfs_mem_err, ret);
 
-	ret = create_and_write(buff, MEM_SIZE);
-	TEST_ASSERT_(ret == -1, "expected error -1 but got %d", ret);
+	ret = create_and_write(buff, MEM_SIZE, &id);
+	TEST_ASSERT_(ret == helfs_mem_err, "expected error helfs_mem_err-%d but got %d", helfs_mem_err, ret);
 
-	ret = create_and_write(buff, MEM_SIZE - sizeof(file) + 1);
-	TEST_ASSERT_(ret == -1, "expected error -1 but got %d", ret);
+	ret = create_and_write(buff, MEM_SIZE - sizeof(file) + 1, &id);
+	TEST_ASSERT_(ret == helfs_mem_err, "expected error helfs_mem_err-%d but got %d", helfs_mem_err, ret);
 }
 
 void write_exact_size_test()
 {
-	int ret;
+	file_id id;
+	helfs_ret ret;
 	char buff[MEM_SIZE];
 	char out_buff[sizeof(buff)];
 	int size_to_write = MEM_SIZE - sizeof(file);
@@ -117,34 +143,36 @@ void write_exact_size_test()
 	ret = init_fs();
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
-	ret = create_and_write(buff, size_to_write);
+	ret = create_and_write(buff, size_to_write, &id);
 	TEST_ASSERT_(ret == 0, "got error - %d", ret);
 
-	ret = read_file(out_buff, size_to_write);
+	ret = read_file(id, out_buff, size_to_write);
 	TEST_ASSERT_(ret == 0, "got error %d", ret);
 
 	TEST_ASSERT(memcmp(buff, out_buff, size_to_write) == 0);
 }
 
-void read_out_of_boundaries()
+void read_out_of_boundaries_test()
 {
-	int ret;
+	file_id id;
+	helfs_ret ret;
 	char buff[100];
 	buff[0] = 0;
 
 	ret = init_fs();
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
-	ret = create_and_write(MY_STR, sizeof(MY_STR));
+	ret = create_and_write(MY_STR, sizeof(MY_STR), &id);
 	TEST_ASSERT_(ret == 0, "got error %d", ret);
 
-	ret = read_file(buff, sizeof(MY_STR) + 1);
-	TEST_ASSERT_(ret == -1, "expected error -1 but got %d", ret);
+	ret = read_file(id, buff, sizeof(MY_STR) + 1);
+	TEST_ASSERT_(ret == helfs_boundaries_err, "expected error helfs_boundaries_err-%d but got %d", helfs_boundaries_err, ret);
 }
 
-void read_part_of_file()
+void read_part_of_file_test()
 {
-	int ret;
+	file_id id;
+	helfs_ret ret;
 	char buff[100];
 	buff[0] = 0;
 	int size_to_read = sizeof(MY_STR) - 1;
@@ -152,20 +180,51 @@ void read_part_of_file()
 	ret = init_fs();
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
-	ret = create_and_write(MY_STR, sizeof(MY_STR));
+	ret = create_and_write(MY_STR, sizeof(MY_STR), &id);
 	TEST_ASSERT_(ret == 0, "got error %d", ret);
 
-	ret = read_file(buff, size_to_read);
+	ret = read_file(id, buff, size_to_read);
 	TEST_ASSERT_(ret == 0, "Got error %d", ret);
 
 	TEST_ASSERT(memcmp(buff, MY_STR, size_to_read) == 0);
 }
 
+void write_read_multiple_files()
+{
+	file_id id1, id2;
+	helfs_ret ret;
+	char buff[100];
+	buff[0] = 0;
+
+	ret = init_fs();
+	TEST_ASSERT_(ret == 0, "Got error %d", ret);
+
+	ret = create_and_write(MY_STR, sizeof(MY_STR), &id1);
+	TEST_ASSERT_(ret == 0, "got error %d", ret);
+
+	ret = create_and_write(MY_STR2, sizeof(MY_STR2), &id2);
+	TEST_ASSERT_(ret == 0, "got error %d", ret);
+
+	ret = read_file(id1, buff, sizeof(MY_STR));
+	TEST_ASSERT_(ret == 0, "got error %d", ret);
+
+	TEST_ASSERT(memcmp(buff, MY_STR, sizeof(MY_STR)) == 0);
+
+	ret = read_file(id2, buff, sizeof(MY_STR2) + 1);
+	TEST_ASSERT_(ret == helfs_boundaries_err, "expected error helfs_boundaries_err-%d but got %d", helfs_boundaries_err, ret);
+
+	ret = read_file(id2, buff, sizeof(MY_STR2));
+	TEST_ASSERT_(ret == 0, "got error %d", ret);
+
+	TEST_ASSERT(memcmp(buff, MY_STR2, sizeof(MY_STR2)) == 0);
+}
+
 TEST_LIST = {
     { "basic-test", basic_test },
-	{ "write_to_big_test", write_to_big_test},
+	{ "write_too_big_test", write_too_big_test},
 	{ "write_exact_size_test", write_exact_size_test},
-	{ "read_out_of_boundaries", read_out_of_boundaries},
-	{ "read_part_of_file", read_part_of_file},
+	{ "read_out_of_boundaries_test", read_out_of_boundaries_test},
+	{ "read_part_of_file_test", read_part_of_file_test},
+	{ "write_read_multiple_files", write_read_multiple_files},
     { NULL, NULL }
 };
