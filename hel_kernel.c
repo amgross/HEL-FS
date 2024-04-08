d1cdcb14d0e789561913c783440d3e0eaa5bea9c
amgross <aryespa@gmail.com>
2024-04-08 21:32:10 +0300

    Add basic functionality of fragmanted file

--- hel_kernel.c
+++ hel_kernel.c
@@ -113,33 +113,94 @@ static hel_ret hel_get_chunks_for_file(int size, hel_file_id **chunks_arr, int *
 		new_file_id =CHUNK_TO_ID(new_file);
 		
 		empty_sectors = hel_count_empty_sectors(new_file_id);
-		if((empty_sectors * SECTOR_SIZE) - sizeof(hel_chunk) >= size)
+		chunks_arr_tmp = (hel_file_id *)realloc(*chunks_arr, (*chunks_num + 1) * sizeof(hel_file_id));
+		if(chunks_arr_tmp == NULL)
 		{
-			chunks_arr_tmp = (hel_file_id *)realloc(*chunks_arr, (*chunks_num + 1) * sizeof(hel_file_id));
-			if(chunks_arr_tmp == NULL)
-			{
-				free(*chunks_arr);
-				return hel_out_of_heap;
-			}
+			free(*chunks_arr);
+			return hel_out_of_heap;
+		}
 
-			*chunks_arr = chunks_arr_tmp;
-			(*chunks_arr)[*chunks_num] = new_file_id;
-			(*chunks_num)++;
+		*chunks_arr = chunks_arr_tmp;
+		(*chunks_arr)[*chunks_num] = new_file_id;
+		(*chunks_num)++;
+
+		int size_in_empty = (empty_sectors * SECTOR_SIZE) - sizeof(hel_chunk);
+		if(size_in_empty >= size)
+		{
 			break;
 		}
 		else
 		{
 			new_file_id += empty_sectors;
+			size -= size_in_empty;
 		}
 	}
 
 	if(new_file == NULL)
 	{
+		free(*chunks_arr);
 		return hel_mem_err;
 	}
 
 	return hel_success;
 }
+
+// size is in-out
+static hel_ret hel_write_to_chunk(int *size, hel_file_id id, char *buff, bool is_first, hel_file_id next_id)
+{
+	hel_chunk *new_file;
+	int empty_sectors;
+	int needed_sectors = ROUND_UP_DEV(*size + sizeof(hel_chunk), SECTOR_SIZE);
+
+	new_file = ID_TO_CHUNK(id);
+
+	empty_sectors = hel_count_empty_sectors(id);
+
+	// TODO, lots of corner cases here to optimize and for power failure protection
+	if(empty_sectors > needed_sectors)
+	{ // Need to split
+		hel_chunk *next_chunk;
+
+		next_chunk = ID_TO_CHUNK(id + needed_sectors);
+		next_chunk->is_file_begin = 0;
+		next_chunk->is_file_end = 0;
+		next_chunk->size = empty_sectors - needed_sectors;
+		empty_sectors = needed_sectors;
+	}
+	
+	// TODO there is corner case where there isn't enough sectors because the size is in bytes
+	if(empty_sectors < needed_sectors)
+	{
+		// This is not end of file, size is in sectors.
+		new_file->size = empty_sectors;
+		new_file->is_file_end = 0;
+		*size = (empty_sectors *SECTOR_SIZE) - sizeof(hel_chunk);
+	}
+	else
+	{
+		// This is end of file, size is in bytes.
+		new_file->size = *size;
+		new_file->is_file_end = 1;
+	}
+
+	if(is_first)
+	{
+		new_file->is_file_begin = 1;
+	}
+	else
+	{
+		new_file->is_file_begin = 0;
+	}
+
+	new_file->next_file_id = next_id;
+	
+	
+	hel_sign_area(new_file, true);
+
+	memcpy(new_file->data, buff, *size);
+
+	return hel_success;
+} 
 
 hel_ret hel_init()
 {
@@ -192,10 +253,8 @@ void print_file(hel_chunk *file)
 
 hel_ret hel_create_and_write(char *in, int size, hel_file_id *out_id)
 {
-	hel_chunk *new_file;
 	hel_file_id *new_file_id_arr;
 	int chunks_num;
-	int empty_sectors;
 	hel_ret ret;
 
 	if(NULL == out_id)
@@ -209,33 +268,27 @@ hel_ret hel_create_and_write(char *in, int size, hel_file_id *out_id)
 		return ret;
 	}
 
-	assert(chunks_num == 1);
+	for(int i = 0; i < chunks_num; i++)
+	{
+		int write_size = size;
+		ret = hel_write_to_chunk(&write_size, new_file_id_arr[i], in, i == 0, (i == chunks_num - 1)? 0: new_file_id_arr[i + 1]);
+		if(ret != hel_success)
+		{
+			// TODO, change when will have power failure mechanism
+			hel_delete(new_file_id_arr[0]);
 
-	new_file = ID_TO_CHUNK(new_file_id_arr[0]);
+			free(new_file_id_arr);
+			return ret;
+		}
 
-	empty_sectors = hel_count_empty_sectors(new_file_id_arr[0]);
-
-	// TODO, lots of corner cases here to optimize and for power failure protection
-	if(empty_sectors > ROUND_UP_DEV(size + sizeof(hel_chunk), SECTOR_SIZE))
-	{ // Need to split
-		int new_chunk_sector_size = ROUND_UP_DEV(size + sizeof(hel_chunk), SECTOR_SIZE);
-		hel_chunk *next_chunk;
-
-		next_chunk = ID_TO_CHUNK(new_file_id_arr[0] + new_chunk_sector_size);
-		next_chunk->is_file_begin = 0;
-		next_chunk->is_file_end = 0;
-		next_chunk->size = empty_sectors - new_chunk_sector_size;
+		in += write_size;
+		size -= write_size;
+		assert((i == chunks_num - 1) || (size > 0));
 	}
-	
-	new_file->size = size;
-	new_file->is_file_begin = 1;
-	new_file->is_file_end = 1;
-	
-	hel_sign_area(new_file, true);
 
-	memcpy(new_file->data, in, size);
+	assert(size == 0);
 	
-	*out_id = CHUNK_TO_ID(new_file);
+	*out_id = new_file_id_arr[0];
 
 	free(new_file_id_arr);
 	
@@ -252,12 +305,25 @@ hel_ret hel_read(hel_file_id id, char *out, int size)
 	}
 
 	// TODO need much more checks to ensure we are not out of boundaries
-	if(CHUNK_DATA_BYTES(read_file) < size)
+	while(size != 0)
 	{
-		return hel_boundaries_err;
+		int read_len = (size > CHUNK_DATA_BYTES(read_file)) ? CHUNK_DATA_BYTES(read_file): size;
+		memcpy(out, read_file->data, read_len);
+		out += read_len;
+		size -= read_len;
+		if(read_file->is_file_end)
+		{
+			if(size != 0)
+			{
+				return hel_boundaries_err;
+			}
+		}
+		else
+		{
+			read_file = ID_TO_CHUNK(read_file->next_file_id);
+		}
 	}
 
-	memcpy(out, read_file->data, size);
 	return 0;
 }
 
