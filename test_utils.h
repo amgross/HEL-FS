5d5613ade035ff44024a730d52bc75fe04e6112b
amgross <aryespa@gmail.com>
2024-04-11 14:42:23 +0300

    make test that power down in middle of writing and make the memory driver write the metadata atomically

--- test_utils.h
+++ test_utils.h
@@ -7,6 +7,7 @@ void mem_driver_init_test(uint32_t size, uint32_t sector_size);
 typedef enum{
 	PD_NONE,
 	PD_BEFORE_OERATION_RANDOMLY,
+    PD_IN_MIDDLE_RANDOMLY,
 }power_down_option;
 extern power_down_option power_down;
 extern int power_down_prob;
